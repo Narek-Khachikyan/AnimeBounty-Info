f55e8a0cf1efe433b7700a0bab6c4eb2d112b4a1
magiccwarss <matem.mayreni@gmail.com>
2023-06-04 18:26:19 +0400

    gh-pages

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import "./reviewCard.scss";
+
 
 const ReviewCard = ({ user, review, reactions }) => {
   const [showFullReview, setShowFullReview] = useState(false);
