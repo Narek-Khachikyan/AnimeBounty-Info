1411290e35a22eaeb8394562cf9e9c17c32e0a95
magiccwarss <matem.mayreni@gmail.com>
2023-06-04 21:53:17 +0400

    lazy loading

--- src/components/FullAnime/FullAnime.jsx
+++ src/components/FullAnime/FullAnime.jsx
@@ -1,22 +1,21 @@
-import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 import { Link, useParams } from "react-router-dom";
-import "./FullAnime.scss"
-import playButton from "../../assets/images/playButton.svg"
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper";
-import 'swiper/css';
+import "swiper/css";
 import "swiper/css/autoplay";
 import ReviewCard from "../ReviewCard/ReviewCard";
-
-
-
+import LazyLoading from "../LazyLoading/LazyLoading";
+import playButton from "../../assets/images/playButton.svg";
+import "./FullAnime.scss";
 
 const FullAnime = () => {
   const [fullAnime, setFullAnime] = useState([]);
-  const [animePictures, setAnimePictures] = useState([])
-  const [episodes, setEpisodes] = useState([])
-  const [reviews, setReviews] = useState([])
+  const [animePictures, setAnimePictures] = useState([]);
+  const [episodes, setEpisodes] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
 
   const fetchFullAnime = useCallback(async () => {
@@ -28,60 +27,79 @@ const FullAnime = () => {
     } catch (error) {
       console.log(error);
     }
-  }, [id])
+  }, [id]);
+
   const fetchAnimePictures = useCallback(async () => {
     try {
-      const response = await axios.get(`https://api.jikan.moe/v4/anime/${id}/pictures`)
-      setAnimePictures(response.data.data)
+      const response = await axios.get(`https://api.jikan.moe/v4/anime/${id}/pictures`);
+      setAnimePictures(response.data.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }, [id])
+  }, [id]);
+
   const fetchEpisodes = useCallback(async () => {
     try {
-      const response = await axios.get(`https://api.jikan.moe/v4/anime/${id}/episodes`)
-      setEpisodes(response.data.data)
+      const response = await axios.get(`https://api.jikan.moe/v4/anime/${id}/episodes`);
+      setEpisodes(response.data.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }, [id])
+  }, [id]);
+
   const fetchReviews = useCallback(async () => {
     try {
-      const response = await axios.get(`https://api.jikan.moe/v4/anime/${id}/reviews`)
-      setReviews(response.data.data)
+      const response = await axios.get(`https://api.jikan.moe/v4/anime/${id}/reviews`);
+      setReviews(response.data.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }, [id])
-  console.log(reviews)
+  }, [id]);
 
   useEffect(() => {
-    fetchFullAnime();
-    fetchAnimePictures()
-    fetchEpisodes()
-    fetchReviews()
-  }, [fetchFullAnime, fetchAnimePictures, fetchEpisodes, fetchReviews]);
+    const fetchData = async () => {
+      try {
+        await Promise.all([
+          fetchFullAnime(),
+          fetchAnimePictures(),
+          fetchEpisodes(),
+          fetchReviews()
+        ]);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
+    fetchData();
+  }, [fetchFullAnime, fetchAnimePictures, fetchEpisodes, fetchReviews]);
 
   return (
     <div className="fullAnime__wrapper pb-10">
       <div className="container">
-        {fullAnime.map(obj => <div className="fullAnime__contnet flex gap-10 pt-10" key={obj.mal_id}>
-          <div className="fullAnime__img">
-            <img src={obj.images.webp.large_image_url} alt="" />
-            <p className="fullAnime__score bg-white text-black text-xl py-1 px-4">{obj.score}</p>
-          </div>
-          <div className="fullAnime__textWrapper">
-            <h3 className="fullAnime__text text-4xl">{obj.title_english}</h3>
-            <p className="fullAnime__episodes text-xl">Episodes : <b>{obj.episodes}</b></p>
-            <p className="FullAnime__status text-xl">Status : <b>{obj.status}</b></p>
-            <p className="FullAnime__year text-xl">Year : <b>{obj.year ? obj.year : <span>registration🥲</span>}</b></p>
-            <p className="FullAnime__rating text-xl">Rating : <b>{obj.rating}</b></p>
-            <p className="FullAnime__from text-xl">From : <b>{obj.aired.from.slice(0, 10)}</b></p>
-            <p className="FullAnime__to text-xl">To : <b>{obj.aired.to.slice(0, 10)}</b></p>
-            <p className="FullAnime__studio text-xl">Studio : <b>{obj.studios.map(studio => <span key={studio.mal_id}>{studio.name}</span>)}</b></p>
-          </div>
-        </div>)}
+        {isLoading ? (
+          <LazyLoading />
+        ) : (
+          fullAnime.map((obj) => (
+            <div className="fullAnime__contnet flex gap-10 pt-10" key={obj.mal_id}>
+              <div className="fullAnime__img">
+                <img src={obj.images.webp.large_image_url} alt="" />
+                <p className="fullAnime__score bg-white text-black text-xl py-1 px-4">{obj.score}</p>
+              </div>
+              <div className="fullAnime__textWrapper">
+                <h3 className="fullAnime__text text-4xl">{obj.title_english}</h3>
+                <p className="fullAnime__episodes text-xl">Episodes: <b>{obj.episodes}</b></p>
+                <p className="FullAnime__status text-xl">Status: <b>{obj.status}</b></p>
+                <p className="FullAnime__year text-xl">Year: <b>{obj.year ? obj.year : <span>registration🥲</span>}</b></p>
+                <p className="FullAnime__rating text-xl">Rating: <b>{obj.rating}</b></p>
+                <p className="FullAnime__from text-xl">From: <b>{obj.aired.from.slice(0, 10)}</b></p>
+                <p className="FullAnime__to text-xl">To: <b>{obj.aired.to.slice(0, 10)}</b></p>
+                <p className="FullAnime__studio text-xl">Studio:{" "}<b>{obj.studios.map(studio => <span key={studio.mal_id}>{studio.name}</span>)}</b></p>
+              </div>
+            </div>
+          ))
+        )}
+
         <div className="animeImages">
           <p className="text-3xl my-8">Images:</p>
           <div className="animeImages__content flex flex-wrap gap-4">
@@ -89,24 +107,33 @@ const FullAnime = () => {
               modules={[Autoplay]}
               spaceBetween={50}
               slidesPerView={5}
-              autoplay={{ delay: 3000 }}>
+              autoplay={{ delay: 3000 }}
+            >
               {animePictures.map((picture, index) => (
                 <SwiperSlide key={index}>
                   <img className="animeImages__image" src={picture.webp.image_url} alt="" />
-                </SwiperSlide>))}
+                </SwiperSlide>
+              ))}
             </Swiper>
           </div>
         </div>
-        {fullAnime.map(obj => (<div key={obj.mal_id} className="fullAnime__trailer mt-4">
-          {obj.trailer.length > 0 ? <p className="text-3xl mb-4">Trailer</p> :
-            <p className="text-3xl mb-4">There is currently no trailer for this anime</p>}
-          <a className="trailer-imgWrapepr" href={obj.trailer.url}>
-            <div className="trailer-imgWrapper">
-              <img className="trailer-img" src={obj.trailer.images.maximum_image_url} alt="" />
-              <img className="playButton" width={50} height={50} src={playButton} alt="" />
-            </div>
-          </a>
-        </div>))}
+
+        {fullAnime.map((obj) => (
+          <div key={obj.mal_id} className="fullAnime__trailer mt-4">
+            {obj.trailer.length > 0 ? (
+              <p className="text-3xl mb-4">Trailer</p>
+            ) : (
+              <p className="text-3xl mb-4">There is currently no trailer for this anime</p>
+            )}
+            <a className="trailer-imgWrapepr" href={obj.trailer.url}>
+              <div className="trailer-imgWrapper">
+                <img className="trailer-img" src={obj.trailer.images.maximum_image_url} alt="" />
+                <img className="playButton" width={50} height={50} src={playButton} alt="" />
+              </div>
+            </a>
+          </div>
+        ))}
+
         <div className="episodes">
           <p className="episodes__text text-3xl my-4">Episodes:</p>
           <div className="episodes__type flex gap-4 items-center">
@@ -116,27 +143,36 @@ const FullAnime = () => {
           <div className="episodes__content">
             <ol className="episode__name">
               <h3 className="text-2xl mb-3">Episode name</h3>
-              {episodes.map(episode => (
+              {episodes.map((episode) => (
                 <Link to={episode.url} key={episode.mal_id}>
-                  <li className={episode.filler ? "fillerEpisode text-xl" : "canonEpisode text-xl"}>{episode.title}</li>
-                </Link>))}
+                  <li className={episode.filler ? "fillerEpisode text-xl" : "canonEpisode text-xl"}>
+                    {episode.title}
+                  </li>
+                </Link>
+              ))}
             </ol>
             <ul className="episode__score justify-self-end">
               <h3 className="text-2xl mb-3">Episode Score</h3>
-              {episodes.map(episode => <li key={episode.mal_id} className="text-xl">{episode.score * 2}</li>)}
+              {episodes.map((episode) => (
+                <li key={episode.mal_id} className="text-xl">
+                  {episode.score * 2}
+                </li>
+              ))}
             </ul>
-
           </div>
         </div>
+
         <div className="reviews mt-5">
           <h3 className="review__title text-3xl mb-5">{reviews.length > 0 ? <span>Review</span> : null}</h3>
           <div className="reviews__content grid grid-cols-1 grid-rows-1 gap-4">
-            {reviews.map(review => <ReviewCard key={review.mal_id} {...review} />)}
+            {reviews.map((review) => (
+              <ReviewCard key={review.mal_id} {...review} />
+            ))}
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default FullAnime;
